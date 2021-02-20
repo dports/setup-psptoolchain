2b2db376a02970370cb1dd60faf3a2072d9d431a
Jose Silva <krystalgamer@users.noreply.github.com>
2021-02-20 18:54:23 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,3 @@ wget https://github.com/krystalgamer/psptoolchain/releases/download/1/toolchain.
 sudo tar -xvf toolchain.tar.xz -C /
 echo "PSPDEV=/usr/local/pspdev" >> $GITHUB_ENV
 echo "/usr/local/pspdev/bin" >> $GITHUB_PATH
-psp-gcc -v
