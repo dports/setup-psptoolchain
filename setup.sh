9042bd00c5813d383183c3967aef4f66713a4974
Wouter Wijsman <wwijsman@live.nl>
2021-02-24 14:09:09 +0000

    Always use lastest release

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-wget https://github.com/krystalgamer/psptoolchain/releases/download/1/toolchain.tar.xz
+wget https://github.com/krystalgamer/psptoolchain/releases/latest/download/toolchain.tar.xz 
 sudo tar -xvf toolchain.tar.xz -C /
 echo "PSPDEV=/usr/local/pspdev" >> $GITHUB_ENV
 echo "/usr/local/pspdev/bin" >> $GITHUB_PATH
