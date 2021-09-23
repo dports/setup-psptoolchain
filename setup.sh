502f65011712e64c5ba240e35846b55d3bfaa588
DartPower <2005369+DartPower@users.noreply.github.com>
2021-09-23 17:33:09 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,7 @@ wget https://github.com/krystalgamer/psptoolchain/releases/download/1/toolchain.
 sudo tar -xvf toolchain.tar.xz -C /
 echo "PSPDEV=/usr/local/pspdev" >> $GITHUB_ENV
 echo "/usr/local/pspdev/bin" >> $GITHUB_PATH
+
+wget https://github.com/pspdev/psplibraries/archive/master.tar.gz
+sudo tar -xvf master.tar.xz -C /usr/local/pspdev/
+sudo /usr/local/pspdev/psplibraries-master/libraries.sh
